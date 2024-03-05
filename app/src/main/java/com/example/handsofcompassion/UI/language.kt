c8f58f43c88344d5f736ea2b45e92d410900b191
Gabriel Levindo <69587938+gabrielslevindo@users.noreply.github.com>
2024-03-05 20:22:49 -0300

    configs.

---
+++ app/src/main/java/com/example/handsofcompassion/UI/language.kt
@@ -0,0 +1,12 @@
+package com.example.handsofcompassion.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.handsofcompassion.R
+
+class language : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_language)
+    }
+}
