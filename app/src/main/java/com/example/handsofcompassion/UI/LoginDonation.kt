b3968865c23c4fe4e8890a983122fb2824b0801a
Gabriel Levindo <69587938+gabrielslevindo@users.noreply.github.com>
2024-03-12 21:29:54 -0300

    more adjusts in application.

---
+++ app/src/main/java/com/example/handsofcompassion/UI/LoginDonation.kt
@@ -0,0 +1,13 @@
+package com.example.handsofcompassion.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.handsofcompassion.R
+
+class LoginDonation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_donation)
+
+    }
+}
--- app/src/main/java/com/example/handsofcompassion/UI/SelectionScreen.kt
+++ app/src/main/java/com/example/handsofcompassion/UI/SelectionScreen.kt
@@ -1,15 +1,32 @@
 package com.example.handsofcompassion.UI
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.handsofcompassion.R
+import com.example.handsofcompassion.databinding.ActivitySelectionScreen2Binding
 
 class SelectionScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySelectionScreen2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_selection_screen2)
+        binding = ActivitySelectionScreen2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDoar.setOnClickListener{
+
+            startLoginDonationActivity()
+
+        }
+
+    }
+
+    private fun startLoginDonationActivity() {
+        val intent = Intent(this, LoginDonation::class.java)
+        startActivity(intent)
     }
 }
